ef681a34ca6864604bc82455b57c578b74b53c4f
James Kingsley <jameslkingsley@outlook.com>
2016-05-27 00:18:42 +0100

    Change select background to white

--- addons/common/UI/Controls/ctrlCombo.hpp
+++ addons/common/UI/Controls/ctrlCombo.hpp
@@ -2,7 +2,7 @@ class MARS_gui_ctrlCombo: MARS_gui_ctrlDefaultText {
     type = CT_COMBO;
     style = ST_LEFT + LB_TEXTURES + ST_NO_RECT;
     colorBackground[] = {0.05,0.05,0.05,1};
-    colorSelectBackground[] = {COLOR_ACTIVE_RGBA};
+    colorSelectBackground[] = {COLOR_TEXT_RGBA};
     colorText[] = {COLOR_TEXT_RGBA};
     colorDisabled[] = {COLOR_TEXT_RGB,0.25};
     colorSelect[] = {0,0,0,1};
@@ -51,7 +51,7 @@ class MARS_gui_ctrlCombo: MARS_gui_ctrlDefaultText {
 
 class MARS_gui_ctrlComboToolbar: MARS_gui_ctrlCombo {
     colorBackground[] = {0.05,0.05,0.05,1};
-    colorSelectBackground[] = {COLOR_ACTIVE_RGBA};
+    colorSelectBackground[] = {COLOR_TEXT_RGBA};
     arrowEmpty = "\a3\3DEN\Data\Controls\ctrlCombo\arrowEmptyToolbar_ca.paa";
     arrowFull = "\a3\3DEN\Data\Controls\ctrlCombo\arrowEmptyToolbar_ca.paa";
     wholeHeight = 12 * SIZE_M * GRID_H;
