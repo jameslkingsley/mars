829295d6a5ba0d7dabd2f5208a3a9ae765eb2c38
James Kingsley <jameslkingsley@outlook.com>
2016-05-30 00:51:27 +0100

    Finish environments attributes config

--- addons/environment/CfgAttributes.hpp
+++ addons/environment/CfgAttributes.hpp
@@ -7,29 +7,17 @@ class GVARMAIN(attributes) {
             class AttributeCategories {
                 class DateTime {
                     class AttributeItems {
-                        class Date {
+                        class DateTest {
                             displayName = CSTRING(Date_Name);
                             tooltipText = CSTRING(Date_Tooltip);
                             class AttributeControls {
-                                class Year {
+                                class Date {
                                     condition = "true";
-                                    type = "COMBO";
-                                    values = "_years = []; for '_i' from 1900 to 2050 do {_years pushBack _i}; _years";
-                                    labels = "_years = []; for '_i' from 1900 to 2050 do {_years pushBack str _i}; _years";
-                                    selected = "date select 0";
-                                    expression = QUOTE([(_this select 1)] call FUNC(setDateTime));
-                                };
-                                class Month: Year {
-                                    values[] = {1,2,3,4,5,6,7,8,9,10,11,12};
-                                    labels[] = {"January","Febuary","March","April","May","June","July","August","September","October","November","December"};
-                                    selected = "date select 1";
-                                    expression = QUOTE([ARR_2(-1, (_this select 1))] call FUNC(setDateTime));
-                                };
-                                class Day: Year {
-                                    values = "_days = []; for '_i' from 1 to 31 do {_days pushBack _i}; _days";
-                                    labels = "_days = []; for '_i' from 1 to 31 do {_days pushBack str _i}; _days";
-                                    selected = "date select 2";
-                                    expression = QUOTE([ARR_3(-1, -1, (_this select 1))] call FUNC(setDateTime));
+                                    type = "DATE";
+                                    year = "date select 0";
+                                    month = "date select 1";
+                                    day = "date select 2";
+                                    expression = QFUNC(setDateTime);
                                 };
                             };
                         };
@@ -43,8 +31,12 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 23};
                                     step = 1;
                                     position = "date select 3";
-                                    expression = QUOTE([ARR_4(-1, -1, -1, (_this select 0))] call FUNC(setDateTime));
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([ARR_4(-1, -1, -1, _this)] call FUNC(setDateTime));
+                                    onSliderPosChanged = "\
+                                        _ctrl = _this select 0;\
+                                        _value = round (sliderPosition _ctrl);\
+                                        _ctrl ctrlSetTooltip (['0' + str _value, str _value] select (_value >= 10))\
+                                    ";
                                 };
                             };
                         };
@@ -58,8 +50,12 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 59};
                                     step = 1;
                                     position = "date select 4";
-                                    expression = QUOTE([ARR_5(-1, -1, -1, -1, (_this select 0))] call FUNC(setDateTime));
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([ARR_5(-1, -1, -1, -1, _this)] call FUNC(setDateTime));
+                                    onSliderPosChanged = "\
+                                        _ctrl = _this select 0;\
+                                        _value = round (sliderPosition _ctrl);\
+                                        _ctrl ctrlSetTooltip (['0' + str _value, str _value] select (_value >= 10))\
+                                    ";
                                 };
                             };
                         };
@@ -77,8 +73,8 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 1};
                                     step = 0.1;
                                     position = "overcast";
-                                    expression = QUOTE([(_this select 0)] remoteExec [ARR_2(QUOTE(QFUNC(setOvercast)), REMOTE_GLOBAL)]);
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([_this] remoteExec [ARR_2(QUOTE(QFUNC(setOvercast)), REMOTE_GLOBAL)]);
+                                    onSliderPosChanged = QUOTE(_ctrl = _this select 0; _ctrl ctrlSetTooltip str([ARR_2(sliderPosition _ctrl, 2)] call EFUNC(common,roundToN)));
                                 };
                             };
                         };
@@ -92,8 +88,8 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 1};
                                     step = 0.1;
                                     position = "rain";
-                                    expression = "";
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([_this] call FUNC(setRain));
+                                    onSliderPosChanged = QUOTE(_ctrl = _this select 0; _ctrl ctrlSetTooltip str([ARR_2(sliderPosition _ctrl, 2)] call EFUNC(common,roundToN)));
                                 };
                             };
                         };
@@ -107,8 +103,8 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 1};
                                     step = 0.1;
                                     position = "lightnings";
-                                    expression = "";
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([_this] call FUNC(setLightning));
+                                    onSliderPosChanged = QUOTE(_ctrl = _this select 0; _ctrl ctrlSetTooltip str([ARR_2(sliderPosition _ctrl, 2)] call EFUNC(common,roundToN)));
                                 };
                             };
                         };
@@ -122,8 +118,8 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 1};
                                     step = 0.1;
                                     position = "waves";
-                                    expression = "";
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([_this] call FUNC(setWaves));
+                                    onSliderPosChanged = QUOTE(_ctrl = _this select 0; _ctrl ctrlSetTooltip str([ARR_2(sliderPosition _ctrl, 2)] call EFUNC(common,roundToN)));
                                 };
                             };
                         };
@@ -141,8 +137,8 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 1};
                                     step = 0.01;
                                     position = "fogParams select 0";
-                                    expression = QUOTE([ARR_3((_this select 0), -1, -1)] remoteExec [ARR_2(QUOTE(QFUNC(setFog)), REMOTE_SERVER)]);
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([ARR_3(_this, -1, -1)] remoteExec [ARR_2(QUOTE(QFUNC(setFog)), REMOTE_SERVER)]);
+                                    onSliderPosChanged = QUOTE(_ctrl = _this select 0; _ctrl ctrlSetTooltip str([ARR_2(sliderPosition _ctrl, 2)] call EFUNC(common,roundToN)));
                                 };
                             };
                         };
@@ -156,8 +152,8 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 1};
                                     step = 0.01;
                                     position = "fogParams select 1";
-                                    expression = QUOTE([ARR_3(-1, (_this select 0), -1)] remoteExec [ARR_2(QUOTE(QFUNC(setFog)), REMOTE_SERVER)]);
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([ARR_3(-1, _this, -1)] remoteExec [ARR_2(QUOTE(QFUNC(setFog)), REMOTE_SERVER)]);
+                                    onSliderPosChanged = QUOTE(_ctrl = _this select 0; _ctrl ctrlSetTooltip str([ARR_2(sliderPosition _ctrl, 2)] call EFUNC(common,roundToN)));
                                 };
                             };
                         };
@@ -171,8 +167,8 @@ class GVARMAIN(attributes) {
                                     range[] = {0, 250};
                                     step = 10;
                                     position = "fogParams select 2";
-                                    expression = QUOTE([ARR_3(-1, -1, (_this select 0))] remoteExec [ARR_2(QUOTE(QFUNC(setFog)), REMOTE_SERVER)]);
-                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str(sliderPosition _ctrl)";
+                                    expression = QUOTE([ARR_3(-1, -1, _this)] remoteExec [ARR_2(QUOTE(QFUNC(setFog)), REMOTE_SERVER)]);
+                                    onSliderPosChanged = "_ctrl = _this select 0; _ctrl ctrlSetTooltip str (floor (sliderPosition _ctrl))";
                                 };
                             };
                         };
