1d058ae1e98cee66cc98a58b085c00fea96d7f20
James Kingsley <jameslkingsley@outlook.com>
2016-06-01 17:26:48 +0100

    Add attributes context for CAManbase

--- addons/editor/CfgContext.hpp
+++ addons/editor/CfgContext.hpp
@@ -1,5 +1,12 @@
 class GVARMAIN(context) {
     class ADDON {
+        class GVAR(attributes) {
+            displayName = "Attributes";
+            requiresPosition = false;
+            condition = QUOTE('CAManBase' countType GVAR(selection) == count GVAR(selection));
+            action = QUOTE([ARR_2('__objects', 'CAManBase')] call EFUNC(attributes,openAttributes));
+            order = 100;
+        };
         class GVAR(debug) {
             displayName = "Debug";
             requiresPosition = false;
