cd768d835801fa4ae878af9747f9a6c0cd7538b2
James Kingsley <jameslkingsley@outlook.com>
2016-06-14 19:35:17 +0100

    Change attribute sandbox config

--- addons/attributes/CfgAttributes.hpp
+++ addons/attributes/CfgAttributes.hpp
@@ -2,12 +2,33 @@ class GVARMAIN(attributes) {
     class ADDON {
         class sandbox {
             displayName = "Attributes Sandbox";
-            actionConfirm = "";
-            actionCancel = "";
             class AttributeCategories {
                 class Positional {
                     class AttributeItems {
-                        class Teleport {
+                        class Vector {
+                            displayName = "Vector";
+                            class AttributeControls {
+                                class Control {
+                                    condition = "true";
+                                    type = "VECTOR";
+                                    value[] = {500,500,500};
+                                    expression = "systemChat str _this";
+                                };
+                            };
+                        };
+                        class Checkbox {
+                            displayName = "Checkbox";
+                            class AttributeControls {
+                                class Control {
+                                    condition = "true";
+                                    type = "CHECKBOX";
+                                    checked = false;
+                                    textPlain = "Checkbox label";
+                                    expression = "";
+                                };
+                            };
+                        };
+                        /*class Teleport {
                             displayName = "Teleport";
                             tooltipText = "Teleport yourself to a desired location";
                             class AttributeControls {
@@ -20,7 +41,7 @@ class GVARMAIN(attributes) {
                                     );
                                 };
                             };
-                        };
+                        };*/
                     };
                 };
             };
