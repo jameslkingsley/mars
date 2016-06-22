128d1ac174091c8f658f5a291b30fa8e0e719c04
James Kingsley <jameslkingsley@outlook.com>
2016-06-22 23:55:10 +0100

    Fix map macro declaration

--- addons/attributes/UI/ctrlMap.hpp
+++ addons/attributes/UI/ctrlMap.hpp
@@ -1,4 +1,4 @@
-#define WINDOW_EDITATTRIBUTES_HAbs (safeZoneH min (100 * GRID_H))
+#define WINDOW_EDITATTRIBUTES_HAbs_MAP (safeZoneH min (100 * GRID_H))
 
 class GVAR(ctrlMapWindow) {
     idd = IDD_EDITATTRIBUTES_CTRLMAP;
@@ -10,20 +10,20 @@ class GVAR(ctrlMapWindow) {
     class ControlsBackground {
         class Background: MARS_gui_ctrlStaticBackground {
             x = QUOTE(CENTER_X - (100 * 0.5) * GRID_W);
-            y = QUOTE(0.5 - WINDOW_EDITATTRIBUTES_HAbs * 0.5 + 10 * GRID_H);
+            y = QUOTE(0.5 - WINDOW_EDITATTRIBUTES_HAbs_MAP * 0.5 + 10 * GRID_H);
             w = QUOTE(100 * GRID_W);
-            h = QUOTE(WINDOW_EDITATTRIBUTES_HAbs - (SIZE_M + 10) * GRID_H);
+            h = QUOTE(WINDOW_EDITATTRIBUTES_HAbs_MAP - (SIZE_M + 10) * GRID_H);
         };
         class BackgroundButtons: MARS_gui_ctrlStaticFooter {
             x = QUOTE(CENTER_X - (100 * 0.5) * GRID_W);
-            y = QUOTE(0.5 + WINDOW_EDITATTRIBUTES_HAbs * 0.5 - (2 * SIZE_M + 2) * GRID_H);
+            y = QUOTE(0.5 + WINDOW_EDITATTRIBUTES_HAbs_MAP * 0.5 - (2 * SIZE_M + 2) * GRID_H);
             w = QUOTE(100 * GRID_W);
             h = (SIZE_M + 2) * GRID_H;
         };
         class Title: MARS_gui_ctrlStaticTitle {
             idc = IDC_EDITATTRIBUTES_CTRLMAP_TITLE;
             x = QUOTE(CENTER_X - (100 * 0.5) * GRID_W);
-            y = QUOTE(0.5 - WINDOW_EDITATTRIBUTES_HAbs * 0.5 + SIZE_M * GRID_H);
+            y = QUOTE(0.5 - WINDOW_EDITATTRIBUTES_HAbs_MAP * 0.5 + SIZE_M * GRID_H);
             w = QUOTE(100 * GRID_W);
             h = SIZE_M * GRID_H;
             text = "Map Position";
@@ -32,7 +32,7 @@ class GVAR(ctrlMapWindow) {
         class ButtonOK: MARS_gui_ctrlButtonOk {
             idc = IDC_EDITATTRIBUTES_CTRLMAP_BTN_OK;
             x = QUOTE(CENTER_X + (100 * 0.5 - 50 - 2) * GRID_W);
-            y = QUOTE(0.5 + WINDOW_EDITATTRIBUTES_HAbs * 0.5 - (2 * SIZE_M + 1) * GRID_H);
+            y = QUOTE(0.5 + WINDOW_EDITATTRIBUTES_HAbs_MAP * 0.5 - (2 * SIZE_M + 1) * GRID_H);
             w = 25 * GRID_W;
             h = SIZE_M * GRID_H;
             text = CSTRING(GUI_BTN_OK);
@@ -42,7 +42,7 @@ class GVAR(ctrlMapWindow) {
         class ButtonCancel: MARS_gui_ctrlButtonCancel {
             idc = IDC_EDITATTRIBUTES_CTRLMAP_BTN_CANCEL;
             x = QUOTE(CENTER_X + (100 * 0.5 - 25 - 1) * GRID_W);
-            y = QUOTE(0.5 + WINDOW_EDITATTRIBUTES_HAbs * 0.5 - (2 * SIZE_M + 1) * GRID_H);
+            y = QUOTE(0.5 + WINDOW_EDITATTRIBUTES_HAbs_MAP * 0.5 - (2 * SIZE_M + 1) * GRID_H);
             w = 25 * GRID_W;
             h = SIZE_M * GRID_H;
             font = FONT2_THIN;
@@ -54,7 +54,7 @@ class GVAR(ctrlMapWindow) {
         class Map: MARS_gui_ctrlMap {
             idc = IDC_EDITATTRIBUTES_CTRLMAP_MAP;
             x = QUOTE((CENTER_X - (100 * 0.5) * GRID_W) + (SIZE_M * GRID_W));
-            y = QUOTE(0.5 - (WINDOW_EDITATTRIBUTES_HAbs * 0.5) + ((SIZE_M * 3) * GRID_H));
+            y = QUOTE(0.5 - (WINDOW_EDITATTRIBUTES_HAbs_MAP * 0.5) + ((SIZE_M * 3) * GRID_H));
             w = QUOTE((100 - (2 * SIZE_M)) * GRID_W);
             h = QUOTE((100 - (2 * SIZE_M)) * GRID_W);
         };
