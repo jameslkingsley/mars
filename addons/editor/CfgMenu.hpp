a55cb2e152989ad9b515b7382fda1f6738b77f4d
James Kingsley <jameslkingsley@outlook.com>
2016-07-03 00:51:26 +0100

    Remove old classes

--- addons/editor/CfgMenu.hpp
+++ addons/editor/CfgMenu.hpp
@@ -1,21 +1,11 @@
 class GVARMAIN(menu) {
     class ADDON {
-        class mission {
-            displayName = "Mission";
-            class clear_map {
-                displayName = "Clear Map";
-                action = "hint 'Clear Map'";
-            };
-            class end_scenario {
-                displayName = "End Scenario";
-                action = "hint 'End Scenario'";
-            };
-        };
         class settings {
             displayName = "Settings";
+            
             class camera {
                 displayName = "Editor";
-                action = QUOTE([] call FUNC(openEditorSettings));
+                action = QFUNC(openEditorSettings);
             };
         };
     };
