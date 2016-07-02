f6deaeae18e371c6e45567fcae3ee2f112864e66
James Kingsley <jameslkingsley@outlook.com>
2016-07-03 00:51:37 +0100

    Shorten action

--- addons/editor/CfgToolbar.hpp
+++ addons/editor/CfgToolbar.hpp
@@ -5,7 +5,7 @@ class GVARMAIN(toolbar) {
             tooltipText = "Edit players";
             iconOn = QPATHTOF(data\Toolbar\player_on_ca.paa);
             iconOff = QPATHTOF(data\Toolbar\player_off_ca.paa);
-            action = QUOTE([_this] call FUNC(toggleEditPlayers));
+            action = QFUNC(toggleEditPlayers);
         };
     };
 };
