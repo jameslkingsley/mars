f5732107ab15003d5b5d9ebb76c0e160fd358cc4
James Kingsley <jameslkingsley@outlook.com>
2016-07-03 00:51:44 +0100

    Remove unused macros

--- addons/editor/script_component.hpp
+++ addons/editor/script_component.hpp
@@ -50,21 +50,7 @@
 #define CONTEXT_OPTION_WIDTH (0.1 * safeZoneW)
 #define CONTEXT_OPTION_HEIGHT (0.025 * safeZoneH)
 
-// #define ICON_FADE_DISTANCE (profileNamespace getVariable [QEGVAR(clientmenu,viewDistance), 2000])
 #define ICON_FADE_DISTANCE 1500
 #define BOX_FADE_DISTANCE 500
 #define ICON_LEADER_HEIGHT 7.5
 #define ICON_HOVER_SIZE 1.5
-
-#define AB_TREE_POS [\
-        (0.85 * safeZoneW + safeZoneX),\
-        (0.159 * safeZoneH + safeZoneY),\
-        (0.15 * safeZoneW),\
-        (safeZoneH - 0.288)\
-    ]
-
-#define DEFAULT_TAB_OPACITY 0.33
-
-#define AB_CATEGORY_OBJECTS ["Ammo","Anomalies","Armory","Backpacks","Camera","Cargo","Communication","Container","Dead_bodies","Emitters","Explosives","Flag","Fortifications","Furniture","Garbage","Helpers","Intel","Items","ItemsHeadgear","ItemsUniforms","ItemsVests","Lamps","Magazines","Market","Military","Mines","Misc","Objects_Airport","Objects_Sports","Objects_VR","Ruins","Shelters","Ship","Signs","Small_items","Static","Structures","Structures_Airport","Structures_Commercial","Structures_Cultural","Structures_Fences","Structures_Industrial","Structures_Infrastructure","Structures_Military","Structures_Slums","Structures_Sports","Structures_Town","Structures_Transport","Structures_Village","Structures_VR","Structures_Walls","Submerged","Support","Targets","Tents","Test","Training","Uniforms","WeaponAccessories","WeaponsHandguns","WeaponsPrimary","WeaponsSecondary","Wreck","Wreck_sub"]
-
-#define AB_CATEGORY_UNITS ["Afroamerican","Air","Armored","Asian","Autonomous","Car","European","Men","MenArmy","MenCIV","MenDiver","MenRACS","MenRecon","MenSage","MenSLA","MenSniper","MenSpecFor","MenStory","MenSupport","MenUrban","MenVR","MenWood","Submarine","Women"]
