70a273da263140a7b3540e7f380ad0e88158f312
James Kingsley <jameslkingsley@outlook.com>
2016-07-03 00:51:00 +0100

    Fix typo, remove old class

--- addons/editor/CfgContext.hpp
+++ addons/editor/CfgContext.hpp
@@ -1,22 +1,11 @@
 class GVARMAIN(context) {
     class ADDON {
-        class Aattributes {
+        class Attributes {
             displayName = "Attributes";
             requiresPosition = false;
             condition = QUOTE('CAManBase' countType GVAR(selection) == count GVAR(selection));
             action = QUOTE([ARR_2('__objects', 'CAManBase')] call AFUNC(openAttributes));
             order = 100;
-        };
-        class Debug {
-            displayName = "Debug";
-            requiresPosition = false;
-            condition = "(true)";
-            class LogSelection {
-                displayName = "Log Selection to RPT";
-                requiresPosition = false;
-                condition = "(true)";
-                action = QUOTE(_this call FUNC(logSelection));
-            };
         };
         class RemoteControl {
             displayName = "Remote Control";
