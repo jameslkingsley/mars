22f5e69325d64b8651c70dc20eb930add39abf89
James Kingsley <jameslkingsley@outlook.com>
2016-07-04 17:32:45 +0100

    Add night vision and vehicle crew toolbar option

--- addons/editor/CfgToolbar.hpp
+++ addons/editor/CfgToolbar.hpp
@@ -7,5 +7,23 @@ class GVARMAIN(toolbar) {
             iconOff = QPATHTOF(data\Toolbar\player_off_ca.paa);
             action = QFUNC(toggleEditPlayers);
         };
+        class NightVision {
+            default = false;
+            tooltipText = "Night Vision";
+            iconOn = QPATHTOF(data\Toolbar\vision_nvg_ca.paa);
+            iconOff = QPATHTOF(data\Toolbar\vision_normal_ca.paa);
+            action = QFUNC(toggleNightVision);
+            watch = QGVAR(nightVisionEnabled);
+        };
+        class VehicleCrew {
+            default = QGVAR(placeVehiclesWithCrew);
+            tooltipText = "Place vehicles with crew";
+            iconOn = QPATHTOF(data\Toolbar\vision_nvg_ca.paa);
+            iconOff = QPATHTOF(data\Toolbar\vision_normal_ca.paa);
+            action = QUOTE(\
+                params [[ARR_2('_enabled', false)]];\
+                GVAR(placeVehiclesWithCrew) = _enabled;\
+            );
+        };
     };
 };
