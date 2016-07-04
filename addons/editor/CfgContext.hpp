a07a440de4941148f35d6d33c86167df26aeab7e
James Kingsley <jameslkingsley@outlook.com>
2016-07-04 17:32:30 +0100

    Disable attributes context option

--- addons/editor/CfgContext.hpp
+++ addons/editor/CfgContext.hpp
@@ -1,12 +1,12 @@
 class GVARMAIN(context) {
     class ADDON {
-        class Attributes {
+        /*class Attributes {
             displayName = "Attributes";
             requiresPosition = false;
             condition = QUOTE('CAManBase' countType GVAR(selection) == count GVAR(selection));
             action = QUOTE([ARR_2('__objects', 'CAManBase')] call AFUNC(openAttributes));
             order = 100;
-        };
+        };*/
         class RemoteControl {
             displayName = "Remote Control";
             requiresPosition = false;
