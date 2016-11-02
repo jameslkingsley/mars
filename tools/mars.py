6cf53a209dc4c3620a03296427fd17e047a69cc0
James Kingsley <jameslkingsley@outlook.com>
2016-11-02 16:47:05 +0000

    Fix #54

--- tools/mars.py
+++ tools/mars.py
@@ -2,62 +2,118 @@ import os
 import sys
 import re
 import subprocess
+import textwrap
 
+PREFIX = 'mars'
 AUTHOR = 'Kingsley'
 ADDONS = '../addons/'
 PREP = 'XEH_PREP.hpp'
 EDITOR = 'C:/Program Files/Sublime Text 3/sublime_text.exe'
 
-def createFunction(addon, name):
-    funcPath = "{}{}/functions/fnc_{}.sqf".format(ADDONS, addon, name)
-    prepPath = "{}{}/{}".format(ADDONS, addon, PREP)
+def getFunctionPath(addon, name):
+    return "{}{}/functions/fnc_{}.sqf".format(ADDONS, addon, name)
 
-    if os.path.exists(funcPath):
-        print("Function already exists, exiting")
-        return
+def getPrepPath(addon):
+    return "{}{}/{}".format(ADDONS, addon, PREP)
 
-    func = open(funcPath, "w")
-    func.write('\
-/*\n\
- * Author: {}\n\
- * \n\
- *\n\
- * Arguments:\n\
- * \n\
- *\n\
- * Return Value:\n\
- * \n\
- *\n\
- * Example:\n\
- * [] call mars_{}_fnc_{};\n\
- *\n\
- * Public: \n\
- */\n\
-\n\
-#include "script_component.hpp"\n\
-\n'.format(AUTHOR, addon, name))
-    func.close()
-
+def appendPrep(addon, name):
+    prepPath = getPrepPath(addon)
     prep = open(prepPath, "a")
     prep.write("PREP({});\n".format(name))
     prep.close()
 
-    subprocess.Popen([EDITOR, funcPath])
+def openFile(file):
+    subprocess.Popen([EDITOR, file])
+
+def createFunction(addon, name, contents=''):
+    function = getFunctionPath(addon, name)
+
+    if os.path.exists(function):
+        print("Function already exists, exiting")
+        return
+
+    file = open(function, "w")
+
+    if contents == '':
+        file.write(textwrap.dedent('''\
+            /*
+             * Author: {0}
+             * 
+             *
+             * Arguments:
+             * 
+             *
+             * Return Value:
+             * 
+             *
+             * Example:
+             * [] call {3}_{1}_fnc_{2};
+             *
+             * Public: 
+             */
+
+            #include "script_component.hpp"
+            ''').format(AUTHOR, addon, name, PREFIX))
+    else:
+        file.write(contents)
+
+    file.close()
+
+    appendPrep(addon, name)
+    openFile(function)
+
+def deleteFunction(addon, name):
+    function = getFunctionPath(addon, name)
+
+    if not os.path.exists(function):
+        print("Function does not exist")
+        return
+
+    os.remove(function)
+
+    file = open(getPrepPath(addon), 'r+')
+    lines = file.readlines()
+    file.seek(0)
+
+    for line in lines:
+        if line != "PREP({0});\n".format(name):
+            file.write(line)
+
+    file.truncate()
+    file.close()
+
+def moveFunction(addon, name, newAddon):
+    contents = ''
+    with open(getFunctionPath(addon, name)) as file:
+        contents = file.read()
+    deleteFunction(addon, name)
+    createFunction(newAddon, name, contents)
+
+def renameFunction(addon, name, newName):
+    contents = ''
+    with open(getFunctionPath(addon, name)) as file:
+        contents = file.read()
+    deleteFunction(addon, name)
+    createFunction(addon, newName, contents)
 
 def main():
     args = sys.argv
-    
-    if len(args) != 4:
-        print('Usage: mars.py -f <addon> <function name>')
-        sys.exit(2)
-    
-    method = args[-3]
-    addon = args[-2]
-    name = args[-1]
 
-    {
-        '-f': createFunction(addon, name)
-    }.get(method, -1)
+    if "-cf" in args:
+        createFunction(args[2], args[3])
+        sys.exit(2)
+
+    if "-df" in args:
+        deleteFunction(args[2], args[3])
+        sys.exit(2)
+
+    if "-rf" in args:
+        renameFunction(args[2], args[3], args[4])
+        sys.exit(2)
+
+    if "-mf" in args:
+        moveFunction(args[2], args[3], args[4])
+        sys.exit(2)
 
 if __name__ == '__main__':
     main()
