ca5c00e701e675cd58a51470a98c36ae81ba82f1
James Kingsley <jameslkingsley@outlook.com>
2016-11-02 18:26:04 +0000

    Add codebase update

--- tools/mars.py
+++ tools/mars.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import glob
 import subprocess
 import textwrap
 
@@ -24,6 +25,53 @@ def appendPrep(addon, name):
 
 def openFile(file):
     subprocess.Popen([EDITOR, file])
+
+def replaceFunctionInCode(oldAddon, oldName, newAddon, newName):
+    for subdir, dirs, files in os.walk(ADDONS):
+        for addon in dirs:
+            files = glob.iglob('{0}{1}/**/*.*'.format(ADDONS, addon), recursive=True)
+            for file in files:
+                if ".sqf" in file or ".cpp" in file or ".hpp" in file:
+                    newContent = ''
+                    with open(file, 'r') as script:
+                        for line in script.readlines():
+                            if re.search(r'EFUNC\({0},[\s\S]*{1}\)'.format(oldAddon, oldName), line, re.IGNORECASE):
+                                newLine = re.sub(
+                                    r'EFUNC\({0},[\s\S]*{1}\)'.format(oldAddon, oldName),
+                                    "EFUNC({0}, {1})".format(newAddon, newName),
+                                    line,
+                                    flags=re.IGNORECASE
+                                )
+
+                                print("Replaced {0} with {1} in {2}".format(
+                                    "EFUNC({0}, {1})".format(oldAddon, oldName),
+                                    "EFUNC({0}, {1})".format(newAddon, newName),
+                                    file
+                                ))
+
+                                newContent += newLine
+                            else:
+                                if addon == newAddon and "func({0})".format(oldName.lower()) in line.lower():
+                                    newLine = line.replace(
+                                        "FUNC({0})".format(oldName),
+                                        "FUNC({0})".format(newName)
+                                    )
+
+                                    print("Replaced {0} with {1} in {2}".format(
+                                        "FUNC({0})".format(oldName),
+                                        "FUNC({0})".format(newName),
+                                        file
+                                    ))
+
+                                    newContent += newLine
+                                else:
+                                    newContent += line
+
+                    # Overwrite contents
+                    f = open(file, "w")
+                    f.write(newContent)
+                    f.close()
+        break
 
 def createFunction(addon, name, contents=''):
     function = getFunctionPath(addon, name)
@@ -88,6 +136,7 @@ def moveFunction(addon, name, newAddon):
         contents = file.read()
     deleteFunction(addon, name)
     createFunction(newAddon, name, contents)
+    replaceFunctionInCode(addon, name, newAddon, name)
 
 def renameFunction(addon, name, newName):
     contents = ''
@@ -95,6 +144,7 @@ def renameFunction(addon, name, newName):
         contents = file.read()
     deleteFunction(addon, name)
     createFunction(addon, newName, contents)
+    replaceFunctionInCode(addon, name, addon, newName)
 
 def main():
     args = sys.argv
@@ -113,6 +163,10 @@ def main():
 
     if "-mf" in args:
         moveFunction(args[2], args[3], args[4])
+        sys.exit(2)
+
+    if "-test" in args:
+        replaceFunctionInCode("editor", "shutdown", "editor", "shutdownEditor")
         sys.exit(2)
 
 if __name__ == '__main__':
