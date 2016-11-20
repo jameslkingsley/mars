4cd11f8d0220d655a284b2f7b0bcec2a7958072d
James Kingsley <jameslkingsley@outlook.com>
2016-11-20 16:32:08 +0000

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,13 +1,13 @@
 name = "Mars";
 picture = "mars_logo.paa";
 actionName = "GitHub";
-action = "https://github.com/jameslkingsley/Mars";
-description = "Mars is designed with large communities in mind. When you're the game master for a mission, you take on a big responsibility to deliver a fun experience to the players. Current implementations for real-time control don't offer enough detail and cause a lot of headaches when trying to do very specific things. The goal of Mars is to create a platform that's extensible and robust allowing communities to create their own additions and customise Mars to suit their needs.";
+action = "https://github.com/marseditor/mars";
+description = "Mars is a total Zeus replacement providing Arma communities with a customisable editor capable of realtime mission creation and enhancement.";
 logo = "mars_logo.paa";
 logoOver = "mars_logo.paa";
 tooltip = "Mars";
 tooltipOwned = "Mars";
-overview = "Mars is designed with large communities in mind. When you're the game master for a mission, you take on a big responsibility to deliver a fun experience to the players. Current implementations for real-time control don't offer enough detail and cause a lot of headaches when trying to do very specific things. The goal of Mars is to create a platform that's extensible and robust allowing communities to create their own additions and customise Mars to suit their needs.";
+overview = "Mars is a total Zeus replacement providing Arma communities with a customisable editor capable of realtime mission creation and enhancement.";
 author = "Kingsley";
 overviewPicture = "mars_logo.paa";
-overviewText = "Mars is designed with large communities in mind. When you're the game master for a mission, you take on a big responsibility to deliver a fun experience to the players. Current implementations for real-time control don't offer enough detail and cause a lot of headaches when trying to do very specific things. The goal of Mars is to create a platform that's extensible and robust allowing communities to create their own additions and customise Mars to suit their needs.";
+overviewText = "Mars is a total Zeus replacement providing Arma communities with a customisable editor capable of realtime mission creation and enhancement.";
