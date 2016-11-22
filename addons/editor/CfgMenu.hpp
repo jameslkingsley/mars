d2df7c62792d8d67c1fa9d998e515246fffd049c
James Kingsley <jameslkingsley@outlook.com>
2016-11-22 19:07:14 +0000

    Add quick-and-dirty end mission

--- addons/editor/CfgMenu.hpp
+++ addons/editor/CfgMenu.hpp
@@ -6,6 +6,10 @@ class GVARMAIN(menu) {
                 displayName = "Hint";
                 action = QUOTE([ARR_3(QQADDON, 'Hint', [ARR_2(140, 90)])] call AFUNC(openAttributes));
             };
+            class EndMission {
+                displayName = "End Mission";
+                action = QUOTE([] remoteExecCall [ARR_2('BIS_fnc_endMission', 0)]);
+            };
         };
         class Settings {
             displayName = "Settings";
