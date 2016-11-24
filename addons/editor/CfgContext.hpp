8c28b26bf3d0a46a42395c8e93f68ab881a8b9f2
James Kingsley <jameslkingsley@outlook.com>
2016-11-24 14:39:14 +0000

    Add teleport command

--- addons/editor/CfgContext.hpp
+++ addons/editor/CfgContext.hpp
@@ -7,9 +7,18 @@ class GVARMAIN(context) {
             action = QUOTE([ARR_2('__objects', 'CAManBase')] call AFUNC(openAttributes));
             order = 100;
         };*/
+        class Teleport {
+            displayName = "Teleport";
+            requiresPosition = 1;
+            action = QUOTE(\
+                params [ARR_2('_units', '_pos')];\
+                {(vehicle _x) setPosASL _pos} forEach _units;\
+            );
+        };
         class RemoteControl {
             displayName = "Remote Control";
-            condition = QUOTE(!isPlayer _this && {count GVAR(selection) == 1} && {count crew vehicle _this > 0});
+            exceptions[] = {"isNotPlayer", "isAlive"};
+            condition = QUOTE(count GVAR(selection) == 1 && {count crew vehicle _this > 0});
             action = QFUNC(remoteControl);
             order = 100;
         };
